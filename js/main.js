36c2ca8675355a269ea816ce55db9920983c3c6d
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-17 23:36:25 -0400

    Indentation

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ grains.forEach(function (item) {
   var $img = $('<img>');
   var $h2 = $('<h2>');
 
-    $ul.append($li);
-    $img.attr('src', 'images/' + item.img);
-    $li.append($img);
-    $h2.html(item.name);
-    $li.append($h2);
-    $p.html(item.desc);
-    $li.append($p);
-    });
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
+  });
